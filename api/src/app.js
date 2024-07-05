4caeb1bbbf6b674900c64278ea64a0004fd0d4f7
david87chdz <david87chdz@gmail.com>
2024-07-05 17:35:03 +0200

    Extract work experience

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const routes = require('./routes/routes.js');
 
 
 app.use(express.json());
-
+app.use(cors());
 // Routes
 app.use('/', routes);
 
--- cvAngular/src/app/components/main/work-experience/work-experience.component.ts
+++ cvAngular/src/app/components/main/work-experience/work-experience.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ExperienceService } from '../../../services/experience.service';
+
 
 @Component({
   selector: 'app-work-experience',
   standalone: true,
   imports: [],
   templateUrl: './work-experience.component.html',
-  styleUrl: './work-experience.component.scss'
+  styleUrl: './work-experience.component.scss',
 })
 export class WorkExperienceComponent {
 
+  public experience: any;
+
+  constructor(private expeinceService$: ExperienceService) {
+    this.getExperience();
+  }
+
+  getExperience() {
+    this.expeinceService$.getExperience().subscribe((data) => {
+      console.log(data);
+      this.experience = data;
+    });
+  }
 }
