2c4d34c3d88a993ca15b03b9b1f8d0b39b631917
david87chdz <david87chdz@gmail.com>
2024-07-09 11:50:45 +0200

    Change styles

--- cvAngular/src/app/components/cv/main/skills/skills.component.ts
+++ cvAngular/src/app/components/cv/main/skills/skills.component.ts
@@ -1,43 +1,97 @@
 import { Component } from '@angular/core';
 import { SkillsService } from '../../../../services/skills.service';
 import { Skill } from '../../../../interfaces/skill';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
   imports: [],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
-  public skillsData: Skill[] = [];
-
-  constructor(private skillsService: SkillsService){
+  private skillsData: Skill[] = [];
+  public languages: Skill[] = [];
+  public frontend: Skill[] = [];
+  public backend: Skill[] = [];
+  public databases: Skill[] = [];
+  public tools: Skill[] = [];
+  public versionControl: Skill[] = [];
+  public markupLanguages: Skill[] = [];
+  constructor(private skillsService: SkillsService) {
     this.getSkillsData();
   }
 
-
-  getSkillsData(){
+  getSkillsData() {
     this.skillsService.getSkillsData().subscribe((data: any) => {
+      map((skill: Skill) => {
+        if (skill.name === 'Spring boot') {
+          skill.name = 'spring';
+        } else if (skill.name === 'PosTgreSql') {
+          skill.name = 'postgres';
+        } else {
+          skill.name = skill.name.toLocaleLowerCase();
+        }
+        return {
+          name: skill.name,
+          level: skill.level,
+        };
+      });
       this.skillsData = data;
       this.mapSkills(this.skillsData);
     });
   }
 
-  mapSkills(skills: Skill[]){
-    return skills.map((skill: Skill) => {
-    
-      if(skill.name === 'Spring boot'){
+  mapSkills(skills: Skill[]) {
+    // Primero, realizamos el mapeo y almacenamos el resultado en una variable
+    const mappedSkills = skills.map((skill: Skill) => {
+      if (skill.name === 'Spring boot') {
         skill.name = 'spring';
-      }else if(skill.name === 'PosTgreSql'){
+      } else if (skill.name === 'PostgreSql') {
         skill.name = 'postgres';
-      } 
-      else{
-        skill.name= skill.name.toLocaleLowerCase();
+      } else {
+        skill.name = skill.name.toLocaleLowerCase();
       }
       return {
         name: skill.name,
-        level: skill.level
+        level: skill.level,
+      };
+    });
+
+    // Luego, llamamos a filterSkills
+    this.filterSkills(this.skillsData);
+
+    // Finalmente, retornamos el resultado del mapeo
+    return mappedSkills;
+  }
+
+  filterSkills(skills: Skill[]) {
+    skills.forEach((skill: Skill) => {
+      switch (true) {
+        case skill.type.includes('programación'):
+          this.languages.push(skill);
+          break;
+        case skill.type.includes('frontend'):
+          this.frontend.push(skill);
+          break;
+        case skill.type.includes('backend'):
+          this.backend.push(skill);
+          break;
+        case skill.type.includes('datos'):
+          this.databases.push(skill);
+          break;
+        case skill.type.includes('desarrollo'):
+          this.tools.push(skill);
+          break;
+        case skill.type.includes('versiones'):
+          this.versionControl.push(skill);
+          break;
+        case skill.type.includes('marcado'):
+          this.markupLanguages.push(skill);
+          break;
+        default:
+          console.log('No se ha encontrado el tipo de skill');
       }
     });
   }
